7e4269ea3bc77e131b1d0754db111dad92eb32ed
kkvaldys <vladbigun677@gmail.com>
2024-02-01 22:37:24 +0200

    Update task-5.js

--- js/task-5.js
+++
@@ -1,9 +0,0 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
-const btn = document.getElementsByClassName("change-color");
-
-btn.addEventListener("click", getRandomHexColor)
