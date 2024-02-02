958f8fe2d50f30135356aa5d67580f19c9b222ec
kkvaldys <vladbigun677@gmail.com>
2024-02-02 17:17:55 +0200

    done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+function createBoxes(amount) {
+  const initialSize = 30;
+  const step = 10;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const size = initialSize + i * step;
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  } else {
+    alert('Please enter a number between 1 and 100.');
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
